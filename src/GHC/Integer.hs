67ccb712e81eaec5b2d9a6c84e42d4f4376c8000
Sebastian Nagel <sebastian.nagel@ncoding.at>
2018-01-26 12:12:45 +0100

    Start with haskell-managed ByteArrays

--- src/GHC/Integer.hs
+++ src/GHC/Integer.hs
@@ -1,27 +1,56 @@
 {-# LANGUAGE CPP                      #-}
+{-# LANGUAGE ExplicitForAll           #-}
 {-# LANGUAGE ForeignFunctionInterface #-}
 {-# LANGUAGE MagicHash                #-}
 {-# LANGUAGE NoImplicitPrelude        #-}
+{-# LANGUAGE RebindableSyntax         #-}
 {-# LANGUAGE RoleAnnotations          #-}
 {-# LANGUAGE UnboxedTuples            #-}
 {-# LANGUAGE UnliftedFFITypes         #-}
-{-# LANGUAGE ExplicitForAll         #-}
 module GHC.Integer where
 
 #include "MachDeps.h"
 
-import GHC.CString
-import GHC.Magic
-import GHC.Prim
-import GHC.Types
+-- import GHC.CString
+import           GHC.Magic
+import           GHC.Prim
+import           GHC.Types
 
--- | Opaque OpenSsl BIGNUM
-data BigNum = BN# Addr#
+wordSize :: Int
+wordSize = I# WORD_SIZE_IN_BITS#
+
+#if WORD_SIZE_IN_BITS == 64
+# define INT_MINBOUND      -0x8000000000000000
+# define INT_MAXBOUND       0x7fffffffffffffff
+# define ABS_INT_MINBOUND   0x8000000000000000
+# define WORD_SIZE_IN_BYTES 8
+# define WORD_SHIFT         3
+#elif WORD_SIZE_IN_BITS == 32
+# define INT_MINBOUND       -0x80000000
+# define INT_MAXBOUND       0x7fffffff
+# define ABS_INT_MINBOUND   0x80000000
+# define WORD_SIZE_IN_BYTES 4
+# define WORD_SHIFT         2
+#else
+# error unsupported WORD_SIZE_IN_BITS config
+#endif
+
+-- | OpenSSL BIGNUM represented by a single 'ByteArray#'. It corresponds to the
+-- 'd' array in libcrypto's bignum_st structure. Length is multiple of Word#,
+-- least-significant first (BN_BITS2 == WORD_SIZE_IN_BITS).
+data BigNum = BN# ByteArray#
+
+-- | Mutable variant of BigNum for internal use.
+data MutableBigNum s = MBN# (MutableByteArray# s)
 
 data BigNumCtx = CTX# Addr#
 
 data Integer = S# !Int#
-             | B# {-# UNPACK #-} !BigNum
+               -- ^ small integer
+             | Bp# {-# UNPACK #-} !BigNum
+               -- ^ positive bignum, > maxbound(Int)
+             | Bn# {-# UNPACK #-} !BigNum
+               -- ^ negative bignum, < minbound(Int)
 
 -- | Construct 'Integer' value from list of 'Int's.
              --
@@ -30,121 +59,168 @@ mkInteger :: Bool   -- ^ sign of integer ('True' if non-negative)
           -> [Int]  -- ^ absolute value expressed in 31 bit chunks, least
                     -- significant first
           -> Integer
-mkInteger _ [I# i] = S# (i `andI#` 0x7FFFFFFF#)
-mkInteger _ is = runS . liftIO $ do
-  bn <- newBN
-  go bn is
-  return $ B# bn
+mkInteger nonNegative is
+  | nonNegative = f is
+  | True = negateInteger (f is)
  where
-  go bn [(I# i)] = setWord bn (int2Word# (i `andI#` 0x7FFFFFFF#))
-  go bn ((I# i):xs) = do
-    go bn xs
-    lshift bn 31#
-    setWord bn (int2Word# (i `andI#` 0x7FFFFFFF#))
+  f [] = S# 0#
+  f (I# i : is') = smallInteger (i `andI#` 0x7fffffff#) `orInteger` shiftLInteger (f is') 31#
 {-# NOINLINE mkInteger #-}
 
 smallInteger :: Int# -> Integer
 smallInteger i# = S# i#
 {-# NOINLINE smallInteger #-}
 
+negateInteger :: Integer -> Integer
+negateInteger (Bn# n) = Bp# n
+negateInteger (S# INT_MINBOUND#) = Bp# (wordToBigNum ABS_INT_MINBOUND##)
+negateInteger (S# i#) = S# (negateInt# i#)
+negateInteger (Bp# bn)
+  | isTrue# (eqBigNumWord# bn ABS_INT_MINBOUND##) = S# INT_MINBOUND#
+  | True = Bn# bn
+{-# NOINLINE negateInteger #-}
+
+orInteger :: Integer -> Integer -> Integer
+orInteger i _ = i
+
+shiftLInteger :: Integer -> Int# -> Integer
+shiftLInteger i _ = i
+
+-- * Functions operating on BigNum
+
+wordToBigNum :: Word# -> BigNum
+wordToBigNum w# = runS $ do
+  mbn <- newBigNum# 1#
+  writeBigNum# mbn 0# w#
+  freezeBigNum# mbn
+
+eqBigNumWord# :: BigNum -> Word# -> Int#
+eqBigNumWord# = undefined
+
+
+-- * Low-level BigNum creation and manipulation
+
+-- | Create a MutableBigNum with given count of words.
+newBigNum# :: Int# -> S s (MutableBigNum s)
+newBigNum# count# s =
+  -- Calculate byte size using shifts, e.g. for 64bit systems:
+  -- total bytes = word count * 8 = word count * 2 ^ 3 = word count << 3
+  case newByteArray# (count# `uncheckedIShiftL#` WORD_SHIFT#) s of
+    (# s', mba# #) -> (# s', MBN# mba# #)
+
+-- | Freeze a MutableBigNum into a BigNum.
+freezeBigNum# :: MutableBigNum s -> S s BigNum
+freezeBigNum# (MBN# mba#) s =
+  case unsafeFreezeByteArray# mba# s of
+    (# s', ba #) -> (# s', BN# ba #)
+
+-- | Write a word to a MutableBigNum at given word index. Size is not checked!
+writeBigNum# :: MutableBigNum s -> Int# -> Word# -> S s ()
+writeBigNum# (MBN# mba#) i w s =
+  let s' = writeWordArray# mba# i w s
+  in  (# s', () #)
+
 -- * Internal functions
 
-newBN :: IO BigNum
-newBN = do
-  (W# w) <- bn_new
-  return $ BN# (unsafeCoerce# w)
+-- newBN :: IO BigNum
+-- newBN = do
+--   (W# w) <- bn_new
+--   return $ BN# (unsafeCoerce# w)
 
--- TODO(SN): @IO Word@ as @IO Addr#@ and @(# State# RealWorld, Addr# #)@ not allowed
--- BIGNUM *BN_new(void)
-foreign import ccall unsafe "BN_new" bn_new :: IO Word
+-- -- TODO(SN): @IO Word@ as @IO Addr#@ and @(# State# RealWorld, Addr# #)@ not allowed
+-- -- BIGNUM *BN_new(void)
+-- foreign import ccall unsafe "BN_new" bn_new :: IO Word
 
-freeBN :: BigNum -> IO ()
-freeBN (BN# addr) = bn_free addr
+-- freeBN :: BigNum -> IO ()
+-- freeBN (BN# addr) = bn_free addr
 
--- void BN_free(BIGNUM *a);
-foreign import ccall unsafe "BN_free" bn_free :: Addr# -> IO ()
+-- -- void BN_free(BIGNUM *a);
+-- foreign import ccall unsafe "BN_free" bn_free :: Addr# -> IO ()
 
-newCtx :: IO BigNumCtx
-newCtx = do
-  (W# w) <- bn_ctx_new
-  return $ CTX# (unsafeCoerce# w)
+-- newCtx :: IO BigNumCtx
+-- newCtx = do
+--   (W# w) <- bn_ctx_new
+--   return $ CTX# (unsafeCoerce# w)
 
--- BN_CTX *BN_CTX_new(void);
-foreign import ccall unsafe "BN_CTX_new" bn_ctx_new :: IO Word
+-- -- BN_CTX *BN_CTX_new(void);
+-- foreign import ccall unsafe "BN_CTX_new" bn_ctx_new :: IO Word
 
-freeCtx :: BigNumCtx -> IO ()
-freeCtx (CTX# addr) = bn_ctx_free addr
+-- freeCtx :: BigNumCtx -> IO ()
+-- freeCtx (CTX# addr) = bn_ctx_free addr
 
--- void BN_CTX_free(BN_CTX *c);
-foreign import ccall unsafe "BN_CTX_free" bn_ctx_free :: Addr# -> IO ()
+-- -- void BN_CTX_free(BN_CTX *c);
+-- foreign import ccall unsafe "BN_CTX_free" bn_ctx_free :: Addr# -> IO ()
 
-bn2dec :: BigNum -> [Char]
-bn2dec (BN# addr) = unpackCString# (bn_bn2dec addr)
+-- bn2dec :: BigNum -> [Char]
+-- bn2dec (BN# addr) = unpackCString# (bn_bn2dec addr)
 
--- char *BN_bn2dec(const BIGNUM *a);
-foreign import ccall unsafe "BN_bn2dec" bn_bn2dec :: Addr# -> Addr#
+-- -- char *BN_bn2dec(const BIGNUM *a);
+-- foreign import ccall unsafe "BN_bn2dec" bn_bn2dec :: Addr# -> Addr#
 
-bn2hex :: BigNum -> [Char]
-bn2hex (BN# addr) = unpackCString# (bn_bn2hex addr)
+-- bn2hex :: BigNum -> [Char]
+-- bn2hex (BN# addr) = unpackCString# (bn_bn2hex addr)
 
--- char *BN_bn2hex(const BIGNUM *a);
-foreign import ccall unsafe "BN_bn2hex" bn_bn2hex :: Addr# -> Addr#
+-- -- char *BN_bn2hex(const BIGNUM *a);
+-- foreign import ccall unsafe "BN_bn2hex" bn_bn2hex :: Addr# -> Addr#
 
-setWord :: BigNum -> Word# -> IO ()
-setWord (BN# addr) w = do
-  x <- bn_set_word addr w
-  case x of
-    1 -> return ()
-    _ -> IO $ fail "BN_set_word failed"
+-- setWord :: BigNum -> Word# -> IO ()
+-- setWord (BN# addr) w = do
+--   x <- bn_set_word addr w
+--   case x of
+--     1 -> return ()
+--     _ -> IO $ fail "BN_set_word failed"
 
--- int BN_set_word(BIGNUM *a, BN_ULONG w);
-foreign import ccall unsafe "BN_set_word" bn_set_word :: Addr# -> Word# -> IO Int
+-- -- int BN_set_word(BIGNUM *a, BN_ULONG w);
+-- foreign import ccall unsafe "BN_set_word" bn_set_word :: Addr# -> Word# -> IO Int
 
-lshift :: BigNum -> Int# -> IO ()
-lshift (BN# a) n
-  | isTrue# (n ==# 0#) = return ()
-  | isTrue# (n ># 0#) = do
-      x <- bn_lshift a a n
-      case x of
-        1 -> return ()
-        _ -> IO $ fail "BN_lshift failed"
-  | isTrue# (n <# 0#) = IO $ fail "BN_lshift negative n"
+-- lshift :: BigNum -> Int# -> IO ()
+-- lshift (BN# a) n
+--   | isTrue# (n ==# 0#) = return ()
+--   | isTrue# (n ># 0#) = do
+--       x <- bn_lshift a a n
+--       case x of
+--         1 -> return ()
+--         _ -> IO $ fail "BN_lshift failed"
+--   | isTrue# (n <# 0#) = IO $ fail "BN_lshift negative n"
 
--- int BN_lshift(BIGNUM *r, const BIGNUM *a, int n);
-foreign import ccall unsafe "BN_lshift" bn_lshift :: Addr# -> Addr# -> Int# -> IO Int
+-- -- int BN_lshift(BIGNUM *r, const BIGNUM *a, int n);
+-- foreign import ccall unsafe "BN_lshift" bn_lshift :: Addr# -> Addr# -> Int# -> IO Int
 
-addBN :: BigNum -> BigNum -> IO BigNum
-addBN (BN# a) (BN# b) = do
-  (BN# r) <- newBN
-  x <- bn_add r a b
-  case x of
-    1 -> return $ BN# r
-    _ -> runS $ fail "BN_add failed"
+-- addBN :: BigNum -> BigNum -> IO BigNum
+-- addBN (BN# a) (BN# b) = do
+--   (BN# r) <- newBN
+--   x <- bn_add r a b
+--   case x of
+--     1 -> return $ BN# r
+--     _ -> runS $ fail "BN_add failed"
 
--- int BN_add(BIGNUM *r, const BIGNUM *a, const BIGNUM *b);
-foreign import ccall unsafe "BN_add" bn_add :: Addr# -> Addr# -> Addr# -> IO Int
+-- -- int BN_add(BIGNUM *r, const BIGNUM *a, const BIGNUM *b);
+-- foreign import ccall unsafe "BN_add" bn_add :: Addr# -> Addr# -> Addr# -> IO Int
 
-mulBN :: BigNum -> BigNum -> IO BigNum
-mulBN (BN# a) (BN# b) = do
-  ctx@(CTX# c) <- newCtx
-  (BN# r) <- newBN
-  x <- bn_mul r a b c
-  freeCtx ctx
-  case x of
-    1 -> return $ BN# r
-    _ -> runS $ fail "BN_mul failed"
+-- mulBN :: BigNum -> BigNum -> IO BigNum
+-- mulBN (BN# a) (BN# b) = do
+--   ctx@(CTX# c) <- newCtx
+--   (BN# r) <- newBN
+--   x <- bn_mul r a b c
+--   freeCtx ctx
+--   case x of
+--     1 -> return $ BN# r
+--     _ -> runS $ fail "BN_mul failed"
 
--- int BN_mul(BIGNUM *r, const BIGNUM *a, const BIGNUM *b, BN_CTX *ctx);
-foreign import ccall unsafe "BN_mul" bn_mul :: Addr# -> Addr# -> Addr# -> Addr# -> IO Int
+-- -- int BN_mul(BIGNUM *r, const BIGNUM *a, const BIGNUM *b, BN_CTX *ctx);
+-- foreign import ccall unsafe "BN_mul" bn_mul :: Addr# -> Addr# -> Addr# -> Addr# -> IO Int
 
 -- Foreign:
 
-type role Ptr phantom
-data Ptr a = Ptr Addr#
-
+-- type role Ptr phantom
+-- data Ptr a = Ptr Addr#
 -- TODO(SN): add a managed Ptr to free on garbage collect (ForeignPtr)
 
--- From integer-gmp:
+{-# INLINE (.) #-}
+(.) :: (b -> c) -> (a -> b) -> a -> c
+f . g = \x -> f (g x)
+
+-- From integer-gmp (requires -XRebindableSyntax):
 -- monadic combinators for low-level state threading
 
 type S s a = State# s -> (# State# s, a #)
@@ -152,10 +228,6 @@ type S s a = State# s -> (# State# s, a #)
 infixl 1 >>=
 infixl 1 >>
 infixr 0 $
-
-{-# INLINE (.) #-}
-(.) :: (b -> c) -> (a -> b) -> a -> c
-f . g = \x -> f (g x)
 
 {-# INLINE ($) #-}
 ($) :: (a -> b) -> a -> b
@@ -193,7 +265,7 @@ runS m = case runRW# m of (# _, a #) -> a
 fail :: [Char] -> S s a
 fail s = return# (raise# s)
 
--- GHC.Err:
+-- From GHC.Err:
 
 undefined :: forall a. a
 undefined = runS $ fail "Prelude.undefined"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,39 +1,113 @@
-import Test.Hspec (hspec, describe, it, shouldBe, Expectation)
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Numeric (showHex)
+import Data.Bits ((.&.))
+import Data.Char (intToDigit)
+import GHC.Prim
 import GHC.Types
+import Test.Hspec (hspec, describe, it, shouldBe, Expectation)
+import Test.Hspec.QuickCheck (prop)
+import Test.QuickCheck hiding ((.&.))
 
 import qualified OpenSSL.GHC.Integer as X -- this library
 import qualified GHC.Integer as Y -- builtin, usually integer-gmp
 
 main :: IO ()
 main = hspec $ do
-    describe "mkInteger" $ do
-      it "Can create Integers" $ do
-        shouldEqualS (X.mkInteger True [0xbb, 0xaa])
-                     (Y.mkInteger True [0xbb, 0xaa])
-        shouldEqualS (X.mkInteger True [0x7fffffff, 0x7fffffff, 0x3f])
-                     (Y.mkInteger True [0x7fffffff, 0x7fffffff, 0x3f])
-    describe "Low level" $
-      it "Can add/mul" $ do
-        a <- X.newBN
-        X.setWord a 0x11##
-        shouldBe ("0x" ++ X.bn2hex a) "0x11"
-        b <- X.newBN
-        X.setWord b 0x02##
-        shouldBe ("0x" ++ X.bn2hex b) "0x02"
-        r <- X.addBN a b
-        shouldBe ("0x" ++ X.bn2hex r) "0x13"
-        X.freeBN r
-        r <- X.mulBN a b
-        shouldBe ("0x" ++ X.bn2hex r) "0x22"
-        X.freeBN r
-        X.freeBN a
-        X.freeBN b
+  -- putStrLn ""
+  -- putStrLn . showHexX $ X.Bn# (X.wordToBigNum 0xabb##)
+  -- putStrLn $ showHexY (-0xabb :: Y.Integer)
+  describe "smallInteger" $ do
+    prop "can convert from random Int#" $ \(I# i) ->
+      showHexX (X.smallInteger i) === showHexY (Y.smallInteger i)
+  describe "mkInteger" $ do
+    it "can create Integers" $ do
+        shouldEqualHex (X.mkInteger True [0xbb, 0xaa])
+                       (Y.mkInteger True [0xbb, 0xaa])
+        shouldEqualHex (X.mkInteger True [0x7fffffff, 0x7fffffff, 0x3f])
+                       (Y.mkInteger True [0x7fffffff, 0x7fffffff, 0x3f])
+    prop "can create random Integers" $ \(b, is) ->
+      let ints = map truncate32pos is
+      in  showHexX (X.mkInteger b ints) === showHexY (Y.mkInteger b ints)
 
-shouldEqualS :: (Show a, Show b) => a -> b -> Expectation
-shouldEqualS a b = show a `shouldBe` show b
+    -- it "Can create Integers" $ do
+    --     shouldEqualHex (X.mkInteger True [0xbb, 0xaa])
+    --                  (Y.mkInteger True [0xbb, 0xaa])
+    --     shouldEqualHex (X.mkInteger True [0x7fffffff, 0x7fffffff, 0x3f])
+    --                  (Y.mkInteger True [0x7fffffff, 0x7fffffff, 0x3f])
+    -- describe "Low level" $
+    --   it "Can add/mul" $ do
+    --     a <- X.newBN
+    --     X.setWord a 0x11##
+    --     shouldBe ("0x" ++ X.bn2hex a) "0x11"
+    --     b <- X.newBN
+    --     X.setWord b 0x02##
+    --     shouldBe ("0x" ++ X.bn2hex b) "0x02"
+    --     r <- X.addBN a b
+    --     shouldBe ("0x" ++ X.bn2hex r) "0x13"
+    --     X.freeBN r
+    --     r <- X.mulBN a b
+    --     shouldBe ("0x" ++ X.bn2hex r) "0x22"
+    --     X.freeBN r
+    --     X.freeBN a
+    --     X.freeBN b
 
--- * Internal functions
 
+showHexY :: Y.Integer -> String
+showHexY i
+  | i >= 0 = showHex i ""
+  | otherwise = "-" ++ (showHex (abs i) "")
+
+showHexX :: X.Integer -> String
+showHexX (X.S# i)
+  | isTrue# (i >=# 0#) = showHex (I# i) ""
+  | otherwise = "-" ++ (showHex (abs (I# i)) "")
+showHexX (X.Bn# bn) = "-" ++ (show bn)
+showHexX (X.Bp# bn) = show bn
+
+-- TODO(SN): decimal show instance
 instance Show X.Integer where
-  show (X.S# i) = show $ I# i
-  show (X.B# bn) = X.bn2dec bn
+  show = showHexX
+
+instance Show X.BigNum where
+  -- Return base16 encoded string of underlying byte array, MSB first
+  show (X.BN# ba) =
+    dropWhile (== '0') $ go (sizeofByteArray# ba -# 1#)
+   where
+    go 0# = word8hex (indexWord8Array# ba 0#)
+    go i# = word8hex (indexWord8Array# ba i#) ++ go (i# -# 1#)
+
+    word8hex :: Word# -> String
+    word8hex w
+      | isTrue# (w `ltWord#` 0xA##) = [wordToDigit w]
+      | True = [wordToDigit ((w `uncheckedShiftRL#` 4#) `and#` 0xF##), wordToDigit (w `and#` 0xF##)]
+
+    wordToDigit w = intToDigit $ I# (word2Int# w)
+
+-- | HSpec expectation using hex representation
+shouldEqualHex :: X.Integer -> Y.Integer -> Expectation
+shouldEqualHex x y = showHexX x `shouldBe` showHexY y
+
+newtype Integers = Integers (X.Integer, Y.Integer) deriving Show
+
+instance Testable Integers where
+  property (Integers (x, y)) =
+    counterexample (showHexX x ++ " /= " ++ showHexY y) (showHexX x == showHexY y)
+
+instance Arbitrary Integers where
+  arbitrary = oneof [small, big]
+
+small :: Gen Integers
+small = do
+  (I# i) <- arbitrary
+  pure $ Integers (X.smallInteger i, Y.smallInteger i)
+
+big :: Gen Integers
+big = do
+  positive <- arbitrary
+  ints <- map truncate32pos <$> arbitrary -- 31bit int chunks
+  pure $ Integers (X.mkInteger positive ints, Y.mkInteger positive ints)
+
+-- Truncate to a positive 32bit integer (required for mkInteger)
+truncate32pos :: Int -> Int
+truncate32pos i = abs i .&. 0x7fffffff
