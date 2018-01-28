ded723fdfc831d2979965a3f6dd2e92768f345c9
Sebastian Nagel <sebastian.nagel@ncoding.at>
2018-01-28 22:22:11 +0100

    Added more benchmarks - switch to run against builtin integer-simple

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -5,8 +5,6 @@ module Main where
 
 import           Criterion.Main
 import           GHC.Prim
-import           Data.Bits           ((.&.))
-import           Test.QuickCheck     hiding ((.&.))
 
 import qualified GHC.Integer         as Y
 import qualified OpenSSL.GHC.Integer as X
@@ -17,9 +15,11 @@ main = defaultMain
     [ bench "Library" $ whnf mkIntegerBench X.mkInteger
     , bench "Builtin" $ whnf mkIntegerBench Y.mkInteger
     ]
-  , bgroup "mkInteger"
-    [ bench "Library" $ whnf (timesSmallBench X.smallInteger) X.timesInteger
-    , bench "Builtin" $ whnf (timesSmallBench Y.smallInteger) Y.timesInteger
+  , bgroup "timesInteger"
+    [ bench "Small Library" $ whnf (timesSmallBench X.smallInteger) X.timesInteger
+    , bench "Small Builtin" $ whnf (timesSmallBench Y.smallInteger) Y.timesInteger
+    , bench "Big Library" $ whnf (timesBigBench X.mkInteger) X.timesInteger
+    , bench "Big Builtin" $ whnf (timesBigBench Y.mkInteger) Y.timesInteger
     ]
   ]
 
@@ -27,7 +27,8 @@ main = defaultMain
 mkIntegerBench :: (Bool -> [Int] -> a) -> a
 mkIntegerBench mkInteger = mkInteger True [0x00 .. 0xff]
 
--- From: haskell-big-integer-experiment
+-- * Benchmarks adapted from: haskell-big-integer-experiment
+
 {-# NOINLINE timesSmallBench #-}
 timesSmallBench :: (Int# -> a) -> (a -> a -> a) -> a
 timesSmallBench smallInteger timesInteger =
@@ -40,3 +41,16 @@ timesSmallBench smallInteger timesInteger =
 
   value = smallInteger 3#
   count = 32      -- 3 ^ 32 < 0x7fffffffffffffff
+
+{-# NOINLINE timesBigBench #-}
+timesBigBench :: (Bool -> [Int] -> a) -> (a -> a -> a) -> a
+timesBigBench mkInteger timesInteger =
+    loop 10 count value
+  where
+    -- loop :: Int -> Int -> Integer -> Integer
+    loop !0 !0 !accum = accum
+    loop !k !0 !_ = loop (k - 1) count value
+    loop !k !j !accum = loop k (j - 1) (timesInteger accum value)
+
+    value = mkInteger True [ 0x300 .. 0x3ff ]
+    count = 20
